cd28dffbd3aab95396a22546ac5a61bb5678460a
Vincent-Garret <vincentgarret33@gmail.com>
2020-04-30 14:59:43 +0200

    fix checkbox

--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -3,12 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Reservation;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
 
 
 
@@ -19,12 +23,28 @@ class ReservationType extends AbstractType
         $builder
             ->add('name')
             ->add('lastName')
-            ->add('number')
+            ->add('number', ChoiceType::class,[
+                'choices' => [1 => 1,
+                    2 => 2,
+                    3 => 3,
+                    4 => 4,
+                    5 => 5,
+                    6 => 6,
+                    7 => 7,
+                    8 => 8],
+                'label' => 'Nombre de personnes',
+                'multiple' => false,
+                'expanded' => true,
+                'required' =>true,
+                'choice_value'=> null,
+            ])
+
             ->add('time', DateTimeType::class,[
                 'placeholder' => [
                     'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
                     'hour' => 'Hour', 'minute' => 'Minute', 'second' => 'Second',
-                ]
+                ],
+                'widget' => 'single_text'
             ])
             ->add('mail')
             ->add('telephone', TelType::class)
